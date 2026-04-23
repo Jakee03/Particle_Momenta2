6be7eccc9094bbf5d95cde8d1422bf6898a7afdd
jakee03 <jakefairham@rocketmail.com>
2026-04-23 23:42:50 +0100

    removed previous assignments cout checks - rewritten main to print particles and test virtual print

--- Particle.cpp
+++ Particle.cpp
@@ -10,31 +10,26 @@
 
 //default constructor
 Particle::Particle() : name("unknown"), mass(0), charge(0), is_antiparticle(false), momentum(std::make_unique<FourMomentum>(0.0, 0.0, 0.0, 0.0)) {
-    std::cout << "Calling Default Constructor" << std::endl;
 }
 
 //parameterized constructor
 Particle::Particle(std::string name, double mass, double charge, bool is_antiparticle, double E, double px, double py, double pz)
     : name(name), mass(mass), charge(charge), is_antiparticle(is_antiparticle), 
       momentum(std::make_unique<FourMomentum>(E, px, py, pz)) {       
-    std::cout << "Calling Parameterized Constructor" << std::endl;
 }
 
 //destructor
 Particle::~Particle() {
-    std::cout << "Calling Destructor" << std::endl;
 }
 
 //copy constructor
 Particle::Particle(const Particle& other)
     : name(other.name), mass(other.mass), charge(other.charge), is_antiparticle(other.is_antiparticle),
       momentum(std::make_unique<FourMomentum>(*other.momentum)) {
-    std::cout << "Calling Copy Constructor" << std::endl;
 }
 
 //copy assignment operator
 Particle& Particle::operator=(const Particle& other) {
-    std::cout << "Calling Copy Assignment Operator" << std::endl;
     if (this == &other) return *this; // self-assignment check
 
     name = other.name;
@@ -52,12 +47,10 @@ Particle& Particle::operator=(const Particle& other) {
 Particle::Particle(Particle&& other) noexcept
  : name(std::move(other.name)), mass(other.mass), charge(other.charge), is_antiparticle(other.is_antiparticle),
    momentum(std::move(other.momentum)) {
-    std::cout << "Calling Move Constructor" << std::endl;
 }
 
 //move assignment operator
 Particle& Particle::operator=(Particle&& other) noexcept {
-    std::cout <<"Calling Move Assignment Operator" << std::endl;
     if (this == &other) return *this;
 
     name = std::move(other.name);
--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-/*
 #include <iostream>
 #include <vector>
 #include <utility>
@@ -7,59 +6,17 @@
 #include "Lepton.h"
 
 int main() {
-    try {
-        //test particles
-        // 2 electrons, 4 muons, 2 taus
-        std::vector<Particle> particles;
-        particles.push_back(Particle("electron", 0.511, 0.1, 0.1, 0.1));
-        particles.push_back(Particle("electron", 0.511, -0.1, 0.2, 0.0));
-        
-        particles.push_back(Particle("muon", 105.7, 10.0, 5.0, 2.0));
-        particles.push_back(Particle("muon", 105.7, -20.0, 10.0, -5.0));
-        particles.push_back(Particle("muon", 105.7, 15.0, -5.0, 3.0));
-        particles.push_back(Particle("muon", 105.7, -10.0, 20.0, -2.0));
-            
-        particles.push_back(Particle("tau", 1776.8, 50.0, -10.0, 5.0));
-        particles.push_back(Particle("tau", 1776.8, 40.0, 15.0, -5.0));
+    //container of base class pointers
+    std::vector<std::unique_ptr<Particle>> particles;
 
-        std::cout << "\nTesting Particle Class Functions:\n" << std::endl;
+    particles.push_back(std::make_unique<Lepton>("Muon", 105.6, -1.0, false, 200, 10, 0, 0));
+    particles.push_back(std::make_unique<Neutrino>("Tau", false, 50, 0, 0, 0));
 
-        //sum 4-mom of the two electrons
-        Particle electron_sum = particles[0] + particles[1];
-        std::cout << "Summed electrons energy: " << electron_sum.getE() << " MeV" << std::endl;
-
-        //dot product of 1st 2 muons
-        double muon_dot = particles[2].dotProduct(particles[3]);
-        std::cout << "Dot product of the first two muons: " << muon_dot << " (MeV^2)" << std::endl;
-
-        //assignment operator test
-        std::cout << "\nTesting Assignment Operator:\n" << std::endl;
-        Particle electronAssign;
-        electronAssign = particles[0];
-        std::cout << "Assigned electron energy: " << electronAssign.getE() << " MeV" << std::endl;
-
-        //copy constructor test 
-        std::cout << "\nTesting Copy Constructor:\n" << std::endl;
-        Particle muonCopy(particles[2]);
-        std::cout << "Copied muon energy: " << muonCopy.getE() << " MeV" << std::endl;
-
-        //move constructor test
-        std::cout << "\nTesting Move Constructor:\n" << std::endl;
-        Particle E("electron", 0.511, 0.5, 0.5, 0.5);
-        Particle movedE(std::move(E));
-
-        //move muon test
-        std::cout << "\nTesting Move Assignment:\n" << std::endl;
-        Particle M("muon", 105.7, 10.0, 5.0, 2.0);
-        Particle targetM;
-        targetM = std::move(M);
-
-        std::cout << "\n End of Tests, starting cleanup...\n" << std::endl;
-
-    } catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
+    std::cout << "Printing Particles: " <<std::endl;
+    for (const auto& p : particles) {
+        p->print();
+        std::cout << "-----------------" << std::endl;
     }
 
     return 0;
 }
-*/
