917b71f58d8a4d79db457a7dbd04dc88a59c4a5d
jakee03 <jakefairham@rocketmail.com>
2026-04-23 22:09:47 +0100

    getter added for flipping lepton number for antiparticles

--- Lepton.cpp
+++ Lepton.cpp
@@ -8,4 +8,10 @@ Lepton::Lepton() : Particle(), lepton_number(1) {}
 Lepton::Lepton(std::string name, double mass, double charge, bool is_antiparticle, double E, double px, double py, double pz)
     : Particle(name, mass, charge, is_antiparticle, E, px, py, pz), lepton_number(1) {}
 
-    
+//destructor
+Lepton::~Lepton() = default;
+
+//getter (lep no. flips for antis)
+int Lepton::get_lepton_number() const {
+    return is_antiparticle ? -lepton_number : lepton_number;
+}
