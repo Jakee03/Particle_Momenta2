6bf1e6615b684cbb8aa62fea020cbcc4f6d9242e
jakee03 <jakefairham@rocketmail.com>
2026-04-25 14:19:24 +0100

    added particles to test in main.cpp

--- main.cpp
+++ main.cpp
@@ -9,8 +9,9 @@ int main() {
     //container of base class pointers
     std::vector<std::unique_ptr<Particle>> particles;
 
-    particles.push_back(std::make_unique<Lepton>("Muon", 105.6, -1.0, false, 200, 10, 0, 0));
-    particles.push_back(std::make_unique<Neutrino>("Tau", false, 50, 0, 0, 0));
+    particles.push_back(std::make_unique<Electron>(false, 100.0, 10.0, 0.0, 0.0));    particles.push_back(std::make_unique<Neutrino>("Tau", false, 50, 0, 0, 0));
+    particles.push_back(std::make_unique<Muon>(true, 200.0, 20.0, 0.0, 0.0));
+    particles.push_back(std::make_unique<Tau>(false, 300.0, 30.0, 0.0, 0.0));
 
     std::cout << "Printing Particles: " <<std::endl;
     for (const auto& p : particles) {
